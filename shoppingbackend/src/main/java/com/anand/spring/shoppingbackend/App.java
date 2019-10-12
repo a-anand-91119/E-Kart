9c03dcdd40f111ee888c730accae23ce85c9eac2
A Anand <a.anand.91119@gmail.com>
2019-10-12 17:49:32 +0530

    Online Shopping Application Setup

---
+++ shoppingbackend/src/main/java/com/anand/spring/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.anand.spring.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
